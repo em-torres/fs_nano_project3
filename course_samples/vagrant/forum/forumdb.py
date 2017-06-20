52ba18d97a6cd072e243672751f63b87f190f18a
Edwin Marte <kdgpro15@gmail.com>
2017-06-19 21:18:57 -0400

    Updating Vagrant to run on every machine and reidenting python files

--- course_samples/vagrant/forum/forumdb.py
+++ course_samples/vagrant/forum/forumdb.py
@@ -4,12 +4,14 @@ import datetime
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
+
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+    """ Return all posts from the 'database', most recent first. """
+    return reversed(POSTS)
+
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+    """Add a post to the 'database' with the current timestamp."""
+    POSTS.append((content, datetime.datetime.now()))
 
 
